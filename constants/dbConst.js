a0b2c284eaa6e0003b9bc8a8b2a9b0f003ec094c
gurmeet <gurmeetkhirwar@mastrolinks.com>
2020-06-22 15:48:34 +0530

    addmongoUrl

--- constants/dbConst.js
+++ constants/dbConst.js
@@ -3,7 +3,7 @@ require ('dotenv').config ();
 
 let dbUrl;
 if (envCheck ()) {
-  dbUrl = process.env.MONGODB_CONNECTION;
+  dbUrl = "mongodb://vito:vito1234@ds113855.mlab.com:13855/vito-database";
 } else {
 	 dbUrl = process.env.MONGODB_CONNECTION ? process.env.MONGODB_CONNECTION : 'mongodb://localhost:27017/dDb';
 }
